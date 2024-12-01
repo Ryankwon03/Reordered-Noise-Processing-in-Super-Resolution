3c27d3b81cc89027da7e9db990f0991115d4b582
Ryan Sungmo Kwon <ryankwon@umich.edu>
2024-12-01 01:26:28 -0500

    Feat: add experiments

---
+++ run_denoise.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python src/diffusion.py \
+    --model_path models/256x256_diffusion.pt \
+    --classifier_path models/256x256_classifier.pt \
+    --attention_resolutions 32,16,8 \
+    --class_cond True \
+    --diffusion_steps 1000 \
+    --image_size 256 \
+    --learn_sigma True \
+    --noise_schedule linear \
+    --num_channels 256 \
+    --num_head_channels 64 \
+    --num_res_blocks 2 \
+    --resblock_updown True \
+    --use_fp16 True \
+    --use_scale_shift_norm True \
+    --classifier_scale 1.0 \
+    --timestep_respacing 250 \
